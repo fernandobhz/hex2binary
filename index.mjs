28f870c35f37e3ff7328c7033ff84b4b1c5dce00
Fernando Reis Guimarães <fernandobhz@gmail.com>
2022-02-09 12:43:43 -0300

    initial commit

---
+++ index.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import path from 'path';
+
+const { log } = console;
+
+const die = (...args) => {
+  log(...args);
+  process.exit(1);
+}
+
+const [nodePath, scriptPath, inputFilePath, outputFilePath] = process.argv;
+
+if (!inputFilePath || !outputFilePath) {
+  die('Usage: npx hex2binary <input-file> <output-file>');
+}
+
+const inputFileContent = await fs.readFile(inputFilePath, 'utf8');
+const parsedInputFileContent = inputFileContent.startsWith('0x') ? inputFileContent.slice(2) : inputFileContent;
+const inputBuffer = Buffer.from(parsedInputFileContent, 'hex');
+await fs.writeFile(outputFilePath, inputBuffer);
+
+log(`Done`);
